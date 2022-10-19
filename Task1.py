73b83d3639fc599a4d569ce83e8d64194b505fcf
vitali_r <rogatsevich.vv@mail.ru>
2022-10-20 01:55:28 +0300

    Task commit

--- Task1.py
+++ Task1.py
@@ -1,62 +1,3 @@
-# import bs4
-# from bs4 import BeautifulSoup
-# import requests
-#
-# info_number = 79
-#
-# while True:
-#     url = ('https://oriencoop.cl/sucursales/' + str(info_number))
-#     html_text = requests.get(url).text
-#     soup = BeautifulSoup(html_text, 'lxml')
-#
-#     if len(html_text):
-#         # находим одно объявление
-#         all_info = soup.find('div', class_='s-dato').find_all('p')
-#
-#         # внизу две строки находят адрес и потом вывожу адрес без тегов и лишнего
-#         address_with_tags = soup.find('div', class_='s-dato').find_next('p')
-#         address = address_with_tags.find('span')  # нужный адрес
-#
-#         name = soup.find('div', class_='s-dato').find_next('h3').text  # нужное имя
-#         phone = all_info[1].find('span').text  # номер телефона
-#         work_time_1 = all_info[2].find('span').find_next('span').text
-#         work_time_2 = all_info[3].find('span').find_next('span').text
-#
-#         adres = soup.find_all('p', class_=False, id=False)
-#
-#         # print(name)
-#         # print(address.string)
-#         # print(phone)
-#         # print(work_time_1)
-#         # print(work_time_2)
-#         try:
-#             print({'name': name, 'address': address.string, 'phone': phone, 'working_hours': work_time_1 + work_time_2})
-#
-#         except Exception:
-#             info_number += 1
-#             continue
-
-
-# print(all_info)
-# print(address)
-# print(adres)
-
-
-# import requests
-# from bs4 import BeautifulSoup
-#
-# r = requests.get('https://oriencoop.cl/sucursales/127')
-# soup = BeautifulSoup(r.content)
-#
-# data = soup.find('div', {'class': 's-dato'}).find_all('p')
-#
-# # print(data)
-# print({'address': data[0].find('span').text, 'name': 'Oriencoop', 'phone': data[1].find('span').text})
-
-
-# working code
-
-import bs4
 import json
 from bs4 import BeautifulSoup
 import requests
@@ -64,14 +5,14 @@ import requests
 url = 'https://oriencoop.cl/sucursales/79'
 html_text = requests.get(url).text
 soup = BeautifulSoup(html_text, 'lxml')
+# решил таким образом перебрать страницы, поскольку их номера были на сайте
 page = [79, 127, 165, 167, 170, 173, 180, 182, 184, 187, 188, 194, 196, 208, 219, 231, 267, 312]
-data_list = []
+datas_list = []
 for el in page:
     url = 'https://oriencoop.cl/sucursales/' + str(el)
-    # print(url)
-
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml')
+
     # находим одно объявление
     all_info = soup.find('div', class_='s-dato').find_all('p')
 
@@ -83,22 +24,13 @@ for el in page:
     work_time_1 = all_info[2].find('span').find_next('span').text  # время работы
     work_time_2 = all_info[3].find('span').find_next('span').text
 
-    # adres = soup.find_all('p', class_=False, id=False)
-
-    # print(name)
-    # print(address.string)
-    # print(phone)
-    # print(work_time_1)
-    # print(work_time_2)
-
-    # print({'name': name, 'address': address.string, 'phone': phone, 'working_hours': work_time_1 + work_time_2})
     new_data = {'name': name, 'address': address.string, 'phone': phone, 'working_hours': work_time_1 + work_time_2}
 
-    data_list.append(new_data)
-    # breakpoint()
-    print(data_list)
-    # def write(data, filename):
-with open('task_1_results.json', 'a', encoding='utf-8') as file:
-    json.dumps(data_list, indent=1, ensure_ascii=False)
+    datas_list.append(new_data)
 
-    # write(new_data, 'task_1_results.json')
+    # вывод в консоль данных
+    print({'name': name, 'address': address.string, 'phone': phone, 'working_hours': work_time_1 + work_time_2})
+
+# запись данных в json файл
+with open('task_1_results.json', 'a', encoding='utf-8') as file:
+    json.dump(datas_list, file, indent=1, ensure_ascii=False)
--- Task2.py
+++ Task2.py
@@ -1,131 +1,51 @@
-import bs4
 import json
 from bs4 import BeautifulSoup
 import requests
-import re
-import re
-from re import sub
-from decimal import Decimal
-import io
-from datetime import datetime
-import pandas as pd
+
 # url сайта, к которому далее будет прибавляться id магазина
 site_name = 'https://som1.ru'
+
 # доступ к сайту только так вышло получить
 res = requests.get('https://som1.ru/shops/', headers={
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 })
+
 # залоговки поместил в переменную, чтобы потом не писать их
 zagolovki = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 }
-ID_CITY = [3215,3325,2015,2014,3349, 3441, 3648, 2011, 3392, 2012, 2010, 3630, 2013, 3384, 3605, 3629, 3633, 3581, 3632, 3645, 3667, 3653]
+ID_CITY = [3215, 3325, 2015, 2014, 3349, 3441, 3648, 2011, 3392, 2012, 2010, 3630, 2013, 3384, 3605, 3629, 3633, 3581,
+           3632, 3645, 3667, 3653]
+
 # тут получаю всю страницу где CITY_ID
+data_list = []
 for id in ID_CITY:
     data = requests.post('https://som1.ru/shops/', data={'CITY_ID': id}, headers=zagolovki)
-
     soup = BeautifulSoup(data.text, 'html.parser')
-    #получаю ссылку на страницу, которая лежит в кнопке
+
+    # получаю ссылку на страницу, которая лежит в кнопке
     button_url = soup.select_one('.shops-button a').get('href')
+
     # прибавляем полученную ссылку к начальному адресу страницы, чтобы получить полную ссылку страницы с информацией магазина
     url = site_name + button_url
+
     # получение страницы магазина
     page_data = requests.get(url, headers=zagolovki)
     soup_page_data = BeautifulSoup(page_data.text, 'html.parser')
     info = soup_page_data.select('.shop-info-table tr')
-    #получение данных о магазине
+
+    # получение данных о магазине
     name = soup_page_data.find('h1').text
     address = info[0].find('div').find_next('td').find_next('td').text
     phone_number = info[1].find('div').find_next('td').find_next('td').text
     working_hours = info[2].find('div').find_next('td').find_next('td').text
 
-    # print(button_url)
-    # print(url)
-    # print(name)
-    # print(address)
-    # print(phone_number)
-    # print(working_hours)
-    #
+    new_data = {'name': name, 'address': address, 'phone_number': phone_number, 'working_hours': working_hours}
+    data_list.append(new_data)
+
+    # вывод данных в консоль
     print({'name': name, 'address': address, 'phone_number': phone_number, 'working_hours': working_hours})
 
-    new_data = {'name': name, 'address': address, 'phone_number': phone_number, 'working_hours': working_hours}
-
-    with open('task_2_results.json', 'a', encoding='utf-8') as file:
-        json.dump(new_data, file, indent=1, ensure_ascii=False)
-
-
-
-# write(new_data, 'task_2_results.json')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# import bs4
-# import json
-# from bs4 import BeautifulSoup
-# import requests
-#
-# import re
-# from re import sub
-# from decimal import Decimal
-# import io
-# from datetime import datetime
-# import pandas as pd
-# # url сайта, к которому далее будет прибавляться id магазина
-# site_name = 'https://som1.ru'
-# # доступ к сайту только так вышло получить
-# res = requests.get('https://som1.ru/shops/', headers={
-#     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-# })
-# # залоговки поместил в переменную, чтобы потом не писать их
-# zagolovki = {
-#     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-# }
-#
-# # тут получаю всю страницу где CITY_ID
-# data = requests.post('https://som1.ru/shops/', data={'CITY_ID': 2014}, headers=zagolovki)
-#
-# soup = BeautifulSoup(data.text, 'html.parser')
-# #получаю ссылку на страницу, которая лежит в кнопке
-# button_url = soup.select_one('.shops-button a').get('href')
-# # прибавляем полученную ссылку к начальному адресу страницы, чтобы получить полную ссылку страницы с информацией магазина
-# url = site_name + button_url
-# # получение страницы магазина
-# page_data = requests.get(url, headers=zagolovki)
-# soup_page_data = BeautifulSoup(page_data.text, 'html.parser')
-# info = soup_page_data.select('.shop-info-table tr')
-# #получение данных о магазине
-# name = soup_page_data.find('h1').text
-# address = info[0].find('div').find_next('td').find_next('td').text
-# phone_number = info[1].find('div').find_next('td').find_next('td').text
-# working_hours = info[2].find('div').find_next('td').find_next('td').text
-#
-# # print(name)
-# # print(address)
-# # print(phone_number)
-# # print(working_hours)
-#
-# print({'name': name, 'address': address, 'phone_number': phone_number, 'working_hours': working_hours})
-#
-#
-# def write(all_data, filename):
-#     all_data = json.dumps(all_data)
-#     all_data = json.loads(str(all_data))
-#     with open(filename, 'w', encoding='utf-8') as file:
-#         json.dump(all_data, file, indent=1, ensure_ascii=False)
-#
-#
-# new_data = {'name': name, 'address': address, 'phone_number': phone_number, 'working_hours': working_hours}
-#
-# write(new_data, 'task_2_results.json')
+# запись данных в json файл
+with open('task_2_results.json', 'a', encoding='utf-8') as file:
+    json.dump(data_list, file, indent=1, ensure_ascii=False)
